c7ffc8058b8f07430b42dfb7d69efb37200720ec
Trenton South <trenton@southfamily.com>
2020-10-05 18:31:13 -0500

    Update Main.java

--- src/higgins/Main.java
+++ src/higgins/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello, my name is Gene");
         System.out.println("Hello Gene, my name is Christine");
         System.out.println("Hello Gene, my name is Michael");
+        System.out.println("Hello from Trenton!");
     }
 }

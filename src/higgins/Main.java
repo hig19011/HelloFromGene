6d1e484dfb42d9e538eb920f3aa3779e4cacd280
mikegibson66 <61169372+mikegibson66@users.noreply.github.com>
2020-10-05 17:29:39 -0600

    Update Main.java
    Added My name

--- src/higgins/Main.java
+++ src/higgins/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Gene");
         System.out.println("Hello Gene, my name is Christine");
+        System.out.println("Hello Gene, my name is Michael");
     }
 }
